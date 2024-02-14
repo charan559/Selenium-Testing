ee0537bf4bd0a88c4f98a1872fe3d0d6fd5fe4ee
Sai Charan Godina (INFOSYS LIMITED) <v-sagodina@microsoft.com>
2024-02-14 11:55:44 +0530

    Selenium Project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Automatio-Selenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Automatio-Selenium")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a85a4f88-70d4-4875-9b60-be5c2d5ac195")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Register.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumUITest
+{
+    [TestClass]
+    public class RegisterWebShop
+    {
+        IWebDriver driver;
+        [TestInitialize]
+        public void Init()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demowebshop.tricentis.com/");
+            driver.Manage().Window.FullScreen();
+        }
+        [TestMethod]
+        public void Registration()
+        {
+            try
+            {
+
+
+                string randomGmail = $"randomuser{Guid.NewGuid().ToString().Substring(0, 8)}@gmail.com";
+                string title = driver.Title;
+                Assert.AreEqual(title, "Demo Web Shop");
+                driver.FindElement(By.ClassName("ico-register")).Click();
+                string registerTitle = driver.Title;
+                Assert.AreEqual(registerTitle, "Demo Web Shop. Register");
+
+                driver.FindElement(By.Id("gender-male")).Click();
+                driver.FindElement(By.Id("FirstName")).SendKeys("Priyanka");
+                driver.FindElement(By.Id("LastName")).SendKeys("kumari");
+                driver.FindElement(By.Id("Email")).SendKeys(randomGmail);
+                driver.FindElement(By.Id("Password")).SendKeys("Pr12345");
+                driver.FindElement(By.Id("ConfirmPassword")).SendKeys("Pr12345");
+                driver.FindElement(By.Id("register-button")).Click();
+
+                string message = driver.FindElement(By.ClassName("result")).Text;
+                Assert.AreEqual(message, "Your registration completed");
+
+                driver.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
